ec91f3f7a791e7b289418e1589e13fd12c586223
Atsushi KOSHIKIZAWA <unconfigured@null.spigotmc.org>
2018-11-23 18:40:00 +0900

    initial commit

---
+++ src/main/kotlin/com/lasta/Hello.kt
@@ -0,0 +1,6 @@
+package com.lasta
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ src/test/kotlin/com/lasta/HelloTest.kt
@@ -0,0 +1,8 @@
+package com.lasta
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
